d3dc5b8e4ab73c2f6878c39ff3f1aeb04369429a
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-09-03 15:41:10 -0500

    Se crean custom media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
+		require('postcss-apply'),
+		require('postcss-custom-media'),
     require('postcss-preset-env')({
+			stage: 1,
 			autoprefixer: {
 				grid: true,
 			}
